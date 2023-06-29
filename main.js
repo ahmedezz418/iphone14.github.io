0600202ba060d972eac8c07ca04e23bf809d10cc
Ahmed Ezzat <aezzahmed28@gmail.com>
2023-06-29 18:48:53 +0300

    iphone14 pro max landing page created

---
+++ main.js
@@ -0,0 +1,9 @@
+function phones(phone) {
+  var img = document.querySelector(".mainImage");
+  img.src = phone;
+}
+
+function colors(color) {
+  var container = document.querySelector(".container");
+  container.style.backgroundColor = color;
+}
